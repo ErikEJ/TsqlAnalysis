66032995187b50a1547b3a29e96160d8f9280d28
Erik Ejlskov Jensen <eej@delegate.dk>
2024-06-08 13:32:52 +0200

    add sample

---
+++ sample/AvoidWaitForDelayRule.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.SqlServer.Dac.CodeAnalysis;
+using Microsoft.SqlServer.Dac.Model;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace SampleRules
+{
+    /// <summary>  
+    /// This is a rule that returns a warning message   
+    /// whenever there is a WAITFOR DELAY statement appears inside a subroutine body.  
+    /// This rule only applies to stored procedures, functions and triggers.  
+    /// </summary>  
+    [ExportCodeAnalysisRule(AvoidWaitForDelayRule.RuleId,
+        "Avoid using WaitFor Delay statements in stored procedures, functions and triggers.",
+        Description = "WAITFOR DELAY statement was found in {0}.",
+        Category = "Performance",
+        RuleScope = SqlRuleScope.Element)]
+    public sealed class AvoidWaitForDelayRule : SqlCodeAnalysisRule
+    {
+        /// <summary>  
+        /// The Rule ID should resemble a fully-qualified class name. In the Visual Studio UI  
+        /// rules are grouped by "Namespace + Category", and each rule is shown using "Short ID: DisplayName".  
+        /// For this rule, that means the grouping will be "SampleRules.Performance", with the rule  
+        /// shown as "SR1004: Avoid using WaitFor Delay statements in stored procedures, functions and triggers."  
+        /// </summary>  
+        public const string RuleId = "SampleRules.SR1004";
+
+        public AvoidWaitForDelayRule()
+        {
+            // This rule supports Procedures, Functions and Triggers. Only those objects will be passed to the Analyze method  
+            SupportedElementTypes = new[]
+            {  
+                  // Note: can use the ModelSchema definitions, or access the TypeClass for any of these types  
+                  ModelSchema.ExtendedProcedure,
+                  ModelSchema.Procedure,
+                  ModelSchema.TableValuedFunction,
+                  ModelSchema.ScalarFunction,
+
+                  ModelSchema.DatabaseDdlTrigger,
+                  ModelSchema.DmlTrigger,
+                  ModelSchema.ServerDdlTrigger
+             };
+        }
+
+        /// <summary>  
+        /// For element-scoped rules the Analyze method is executed once for every matching   
+        /// object in the model.   
+        /// </summary>  
+        /// <param name="ruleExecutionContext">The context object contains the TSqlObject being   
+        /// analyzed, a TSqlFragment  
+        /// that's the AST representation of the object, the current rule's descriptor, and a   
+        /// reference to the model being  
+        /// analyzed.  
+        /// </param>  
+        /// <returns>A list of problems should be returned. These will be displayed in the Visual   
+        /// Studio error list</returns>  
+        public override IList<SqlRuleProblem> Analyze(
+            SqlRuleExecutionContext ruleExecutionContext)
+        {
+            List<SqlRuleProblem> problems = new List<SqlRuleProblem>();
+
+            TSqlObject modelElement = ruleExecutionContext.ModelElement;
+
+            // this rule does not apply to inline table-valued function  
+            // we simply do not return any problem in that case.  
+            if (IsInlineTableValuedFunction(modelElement))
+            {
+                return problems;
+            }
+
+            string elementName = GetElementName(ruleExecutionContext, modelElement);
+
+            // The rule execution context has all the objects we'll need, including the   
+            // fragment representing the object,  
+            // and a descriptor that lets us access rule metadata  
+            TSqlFragment fragment = ruleExecutionContext.ScriptFragment;
+            RuleDescriptor ruleDescriptor = ruleExecutionContext.RuleDescriptor;
+
+            // To process the fragment and identify WAITFOR DELAY statements we will use a   
+            // visitor   
+            WaitForDelayVisitor visitor = new WaitForDelayVisitor();
+            fragment.Accept(visitor);
+            IList<WaitForStatement> waitforDelayStatements = visitor.WaitForDelayStatements;
+
+            // Create problems for each WAITFOR DELAY statement found   
+            // When creating a rule problem, always include the TSqlObject being analyzed. This   
+            // is used to determine  
+            // the name of the source this problem was found in and a best guess as to the   
+            // line/column the problem was found at.  
+            //  
+            // In addition if you have a specific TSqlFragment that is related to the problem   
+            //also include this  
+            // since the most accurate source position information (start line and column) will   
+            // be read from the fragment  
+            foreach (WaitForStatement waitForStatement in waitforDelayStatements)
+            {
+                SqlRuleProblem problem = new SqlRuleProblem(
+                    String.Format(CultureInfo.CurrentCulture,
+                        ruleDescriptor.DisplayDescription, elementName),
+                    modelElement,
+                    waitForStatement);
+                problems.Add(problem);
+            }
+            return problems;
+        }
+
+        private static string GetElementName(
+            SqlRuleExecutionContext ruleExecutionContext,
+            TSqlObject modelElement)
+        {
+            // Get the element name using the built in DisplayServices. This provides a number of   
+            // useful formatting options to  
+            // make a name user-readable  
+            var displayServices = ruleExecutionContext.SchemaModel.DisplayServices;
+            string elementName = displayServices.GetElementName(
+                modelElement, ElementNameStyle.EscapedFullyQualifiedName);
+            return elementName;
+        }
+
+        private static bool IsInlineTableValuedFunction(TSqlObject modelElement)
+        {
+            return TableValuedFunction.TypeClass.Equals(modelElement.ObjectType)
+                           && FunctionType.InlineTableValuedFunction ==
+                modelElement.GetMetadata<FunctionType>(TableValuedFunction.FunctionType);
+        }
+    }
+}
---
+++ sample/WaitForDelayVisitor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.SqlServer.TransactSql.ScriptDom;
+using System.Collections.Generic;
+
+namespace SampleRules
+{
+    internal class WaitForDelayVisitor : TSqlConcreteFragmentVisitor
+    {
+        public IList<WaitForStatement> WaitForDelayStatements { get; private set; }
+
+        public WaitForDelayVisitor()
+        {
+            WaitForDelayStatements = new List<WaitForStatement>();
+        }
+
+        public override void ExplicitVisit(WaitForStatement node)
+        {
+            // We are only interested in WAITFOR DELAY occurrences  
+            if (node.WaitForOption == WaitForOption.Delay)
+                WaitForDelayStatements.Add(node);
+        }
+    }
+}
