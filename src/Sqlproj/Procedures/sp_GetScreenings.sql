8b58c10f40fe779102bcdda03cce4fe580409c79
Erik Ejlskov Jensen <eej@delegate.dk>
2024-06-08 13:42:50 +0200

    update sproc

--- src/Sqlproj/Procedures/sp_GetScreenings.sql
+++ src/Sqlproj/Procedures/sp_GetScreenings.sql
@@ -1,6 +1,7 @@
 ﻿CREATE PROCEDURE [dbo].[sp_GetScreenings]
 	@day DATE
 AS
+	WAITFOR DELAY '00:00:01';
 	SELECT * FROM Screening WITH (NOLOCK)
 	INNER JOIN Movie ON Movie.Id = Screening.MovieId
 	WHERE CAST([ScreeningDate ★] AS DATE) = @day 
